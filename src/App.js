3d20f3b1c5ba4609cadd5771f598b40024a6e962
usama hussain <usamahussain281999@gmail.com>
2020-09-14 23:39:51 +0500

    nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Input, Menu, Button } from 'semantic-ui-react'
+import { Navbar, Nav, Form, FormControl } from 'react-bootstrap';
 import Router from './config/Router';
 import { firebase } from './config/Firebase';
-import { Button } from 'semantic-ui-react'
 import { Provider } from 'react-redux'
 import { store, persistor } from './store/index'
 import { PersistGate } from 'redux-persist/integration/react'
@@ -28,12 +29,22 @@ function App() {
     <div >
       <Provider store={store}>
         <PersistGate persistor={persistor}>
+
           <div className="head" >
             {isLoggedIn && !isLoading &&
               <div>
-                <h5>{isLoggedIn.userEmail}</h5>
-                <Button secondary onClick={() => firebase.auth().signOut()} >Sign Out</Button>
+                <Navbar bg="light" variant="light">
+                  <Navbar.Brand href="#home">Q APP </Navbar.Brand>
+                  <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                  </Nav>
+                  <Form inline>
+                    <p>Welcome <span style={{ color: 'black', fontWeight: 'bold' }}>{isLoggedIn.userEmail}</span></p>
+                    <Button secondary onClick={() => firebase.auth().signOut()} >Sign Out</Button>
+                  </Form>
+                </Navbar>
               </div>}
+
             <Router isLoggedIn={isLoggedIn} isLoading={isLoading} />
           </div >
         </PersistGate>
